9718290d40249dc1788a9300dbab92cb8dac22cc
itsparasbisht <iamparasbisht@gmail.com>
2024-01-07 12:49:23 +0530

    initial setup

---
+++ script.js
@@ -0,0 +1,6 @@
+const map = L.map("map").setView([29.642, 79.7505], 16);
+
+L.tileLayer("http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}", {
+  maxZoom: 20,
+  subdomains: ["mt0", "mt1", "mt2", "mt3"],
+}).addTo(map);

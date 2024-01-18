4c5eab264d8c67f30c09a3b5db1c31b4fb7b3149
itsparasbisht <iamparasbisht@gmail.com>
2024-01-18 19:55:26 +0530

    leaflet compare maps

---
+++ script7.js
@@ -0,0 +1,19 @@
+const map = L.map("map", {
+  center: [28.2096, 83.9856],
+  zoom: 8,
+});
+
+const osmLayer = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+const googleSat = L.tileLayer(
+  "http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
+  {
+    maxZoom: 20,
+    subdomains: ["mt0", "mt1", "mt2", "mt3"],
+  }
+).addTo(map);
+
+L.control.sideBySide(osmLayer, googleSat).addTo(map);
